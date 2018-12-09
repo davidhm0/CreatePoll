c83bb583e2e5ae6a4b8d1056b50ee5f884e8aca5
davidhm0 <davidhm0@yahoo.es>
2018-12-09 02:44:47 +0100

    Initial commit

---
+++ app/src/main/java/edu/uoc/pqtm2018/team2/createpoll/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.uoc.pqtm2018.team2.createpoll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
